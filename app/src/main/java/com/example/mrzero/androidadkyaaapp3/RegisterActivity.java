ef269aca603f075d9aefd58264ac53a7f8a3a28f
Alex <aliabuharb8530@gmail.com>
2019-07-28 01:16:52 +0300

    Add Signup activity

---
+++ app/src/main/java/com/example/mrzero/androidadkyaaapp3/RegisterActivity.java
@@ -0,0 +1,20 @@
+package com.example.mrzero.androidadkyaaapp3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+
+
+    }
+}
