b56ddf48b20585a30c3cae5d7c5c170368dac9ae
Alex <aliabuharb8530@gmail.com>
2019-07-29 11:45:53 +0300

    Add personal activity

---
+++ app/src/main/java/com/example/mrzero/androidadkyaaapp3/MatrialActivity.java
@@ -0,0 +1,27 @@
+package com.example.mrzero.androidadkyaaapp3;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MatrialActivity extends AppCompatActivity   {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matrial);
+
+
+
+
+    }
+
+}
