731613f8500719e3a583d3df7e6457a0364c0cac
Alex <aliabuharb8530@gmail.com>
2019-08-01 10:01:30 +0300

    Add shape btn answer for button in challenge fragment

--- app/src/androidTest/java/com/example/mrzero/androidadkyaaapp3/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/mrzero/androidadkyaaapp3/ExampleInstrumentedTest.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 /**
- * Instrumented test, which will execute on an Android device.
+ * Instrumented shape_btn_answer, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
@@ -18,7 +18,7 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
-        // Context of the app under test.
+        // Context of the app under shape_btn_answer.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.mrzero.androidadkyaaapp3", appContext.getPackageName());
